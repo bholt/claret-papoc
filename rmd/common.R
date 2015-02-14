769311a9f7eb1b625682f87ab899a7ce2438cfe9
Brandon Holt <bholt@cs.washington.edu>
2015-02-14 09:28:03 -0800

    incorporate latest plots: update throughput & add stress throughput

--- rmd/common.R
+++ rmd/common.R
@@ -4,6 +4,15 @@ suppressPackageStartupMessages(require(ggplot2))
 suppressPackageStartupMessages(require(reshape))
 options(RMySQL.dbname="claret") # (rest comes from $HOME/.my.cnf)
 
+library(jsonlite)
+library(scales)
+
+json.to.df <- function(jstr) {
+  d <- fromJSON(jstr)
+  return(data.frame(x=names(d),y=unlist(d)))
+}
+
+
 db <- function(query, factors=c(), numeric=c()) {
   d <- sqldf(query)
   d[factors] <- lapply(d[factors], factor)
@@ -12,6 +21,13 @@ db <- function(query, factors=c(), numeric=c()) {
 }
 
 num <- function(var) as.numeric(as.character(var))
+
+
+df.histogram <- function(json, version="none") {
+  d <- fromJSON(json)
+  return(data.frame(x=num(names(d)), y=num(unlist(d)), version=version))
+}
+
 
 save <- function(g, name=FILE_BASE, file=sprintf("%s/%s.pdf",FILE_DIR,name), w=3.3, h=3.1) {
   ggsave(plot=g, filename=file, width=w, height=h)
@@ -36,14 +52,55 @@ geom_meanbar <- function(labeller=label_pretty) {
   ))
 }
 
+c.blue   <- "#0072B2"
+c.yellow <- "#E69F00"
+c.green  <- "#009E73"
+c.red    <- "#D55E00"
+c.pink   <- "#CC79A7"
+c.gray   <- "#999999"
+
+my_palette <- c(
+  'rw'=c.yellow,
+  'simple'=c.blue,
+  
+  'reader/writer'=c.yellow,
+  'commutative'=c.blue,
+  
+  'follow'=c.blue,
+  'newuser'=c.yellow,
+  'post'=c.green,
+  'repost'=c.red,
+  'timeline'=c.pink,
+  
+  'kronecker'=c.blue,
+  
+  'read'=c.pink,
+  'write'=c.green,
+  
+  'none'=c.gray  
+)
+
 # The palette with grey:
 cbPalette <- c("#0072B2", "#E69F00", "#009E73", "#D55E00", "#CC79A7", "#56B4E9", "#F0E442", "#999999")
 
-theme_mine <- list(
-  # To use for fills, add
-  scale_fill_manual(values=cbPalette),
+my_colors <- function(title="") list(
+  scale_fill_manual(values=my_palette, name=title),
   # To use for line and point colors, add
-  scale_colour_manual(values=cbPalette),
+  scale_color_manual(values=my_palette, name=title)
+)
+
+cc_scales <- function(field=cc, title="Concurrency control:") list(
+  scale_fill_manual(values=my_palette, name=title),
+  scale_color_manual(values=my_palette, name=title),
+  scale_linetype_manual(name=title, values=c('commutative'=1,'reader/writer'=2))
+)
+
+
+theme_mine <- list(
+  scale_fill_manual(values=my_palette),
+  # To use for line and point colors, add
+  scale_color_manual(values=my_palette),
+  # To use for fills, add
   # basic black and white theme
   theme(
     panel.background = element_rect(fill="white"),
@@ -56,6 +113,46 @@ theme_mine <- list(
     panel.grid = element_line(colour="black"),
     axis.text.y = element_text(colour="black"),
     axis.text.x = element_text(colour="black"),
-    text = element_text(size=9, family="Helvetica")
+    text = element_text(size=10, family="Helvetica")
   )
 )
+
+
+capply <- function(col, func) unlist(lapply(col, func))
+
+data <- function(d) {
+  d$failure_rate <- d$txn_failed / (d$txn_count + d$txn_failed)
+  d$throughput <- d$txn_count * num(d$nclients) / d$total_time
+  # d$throughput <- d$ntxns * num(d$nclients) / d$total_time
+  d$avg_latency_ms <- d$txn_time / d$txn_count * 1000
+  
+  d$prepare_total <- d$prepare_retries + d$txn_count
+  d$prepare_retry_rate <- d$prepare_retries / d$prepare_total
+  
+  d$ccf <- factor(d$ccmode, levels=c('simple','rw','bottom'))
+  
+  d$cc <- factor(revalue(d$ccmode, c(
+    # 'bottom'='base',
+    'rw'='reader/writer',
+    'simple'='commutative'
+  )), levels=c('commutative','reader/writer','base'))
+  d$`Concurrency Control` <- d$cc
+  
+  d$Graph <- capply(d$gen, function(s) gsub('kronecker:.+','kronecker',s))
+  
+  d$workload <- factor(revalue(d$mix, c(
+    'geom_repost'='repost-heavy',
+    'read_heavy'='read-heavy',
+    'update_heavy'='mixed'
+  )), levels=c('repost-heavy','read-heavy','mixed'))
+  
+  d$zmix <- sprintf('%s/%s', d$mix, d$alpha)
+  
+  d$facet <- sprintf('shards: %d\n%s\n%d users\n%s', num(d$nshards), d$zmix, d$initusers, d$Graph)
+
+  d$gen_label <- sprintf('%d users\n%s', d$initusers, d$Graph)
+  
+  return(d)
+}
+
+
